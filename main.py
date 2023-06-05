aa53c9ab2e30836a0aab3862186ba36f0fb25676
jjunysgit <061284yong@gmail.com>
2023-06-06 01:09:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import streamlit as st
 
-st.text('hello streamlit')
+def main():
+    st.title("sibal")
+    st.text('sibal')
+    st.header('sibal')
+    st.subheader('sibal')
+
+    st.markdown('# zzzz')
+    st.markdown('# zzzz')
+    st.markdown('# zzzz')
+
+    st.success("success")
+    st.warning("warning")
+    st.info('info')
+    st.error('errror')
+    st.exception('exception')
+
+
+if __name__ =='__main__' :
+    main()
